9a19af309e8bf4b7aa47e1150e1975b98791a739
Michael Benz <michael@benz.io>
2020-01-20 23:58:23 +1100

    Added looping to see what we're dealing with

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dinnerly import Dinnerly
 import pprint
 import argparse
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-u' ,'--username', type=str,
@@ -17,6 +18,19 @@ password = args.password
 dinnerly = Dinnerly(username, password)
 dinnerly.login()
 recipies = dinnerly.getCurrentRecpies()
+
+recipies = sorted(recipies, key = lambda i: datetime.strptime(i['delivery_date'], "%Y-%m-%d")) 
+
+required = set()
+
+for week in recipies:
+    print(week["delivery_date"])
+    for recipie in week["recipes"]:
+        detail = dinnerly.getDetailedRecipe(recipie["id"])
+        for requiredStuff in detail["assumed_ingredients"]:
+            required.add(requiredStuff["name"])
+
+pprint.pprint(required)
 
 recipie = dinnerly.getDetailedRecipe(recipies[0]["recipes"][0]["id"])
 pprint.pprint(recipie)
