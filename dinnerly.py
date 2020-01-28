165a349606b6790bc49d8d02e50b82c9bf715d8a
Michael Benz <michael@benz.io>
2020-01-28 20:44:13 +1100

    Added Current Week function

--- dinnerly.py
+++ dinnerly.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from datetime import date
+from datetime import timedelta
+from datetime import datetime
 
 class Dinnerly():
 
@@ -35,6 +38,7 @@ class Dinnerly():
         user_id_re = re.compile(r'gon\.current_user_id=(.+?);')
         result = user_id_re.search(r.text)
         self.userId = result.group(1)
+        self.user = self.getUser()
 
         return
 
@@ -46,12 +50,44 @@ class Dinnerly():
 
         return self.session.get(url, headers=headers)
     
+    def getUser(self):
+        url = f"https://api.dinnerly.com/users/{self.userId}?brand=dn&country=au&product_type=web"
+        
+        r = self._get(url)
+
+        return r.json()
+    
     def getCurrentRecpies(self):
         url = f"https://api.dinnerly.com/users/{self.userId}/orders/current?brand=dn&country=au&product_type=web"
 
         r = self._get(url)
 
         return r.json()
+    
+    def getThisWeeksRecpies(self):
+        recipies = self.getCurrentRecpies()
+
+        # Sort recipies by date
+        recipies = sorted(recipies, key = lambda i: datetime.strptime(i['delivery_date'], "%Y-%m-%d")) 
+
+        # remove any recipies from before this weeks's run
+        today = date.today()
+
+        deliveryDay = self.user["plan"]["wday"]
+
+        # python starts from 0 for days of the week
+        deliveryDay = deliveryDay - 1 % 7
+
+        offset = (today.weekday() - deliveryDay) % 7
+        mostRecentDeliveryDate = today - timedelta(days=offset)
+        print (mostRecentDeliveryDate)
+
+
+        for recipie in recipies:
+            recipieWeek = datetime.strptime(recipie['delivery_date'], "%Y-%m-%d").date()
+            if recipieWeek == mostRecentDeliveryDate:
+                return recipie
+
     
     def getDetailedRecipe(self, recipeId):
         url = f"https://api.dinnerly.com/recipes/{recipeId}?brand=dn&country=au&product_type=web"
